43535ca2884fc5d93106e2b5dfd99caa2cde0c70
MitchellTGolden <70725595+MitchellTGolden@users.noreply.github.com>
2020-11-29 11:15:54 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> mix = new List<object>();
+            mix.Add(7);
+            mix.Add(28);
+            mix.Add(-1);
+            mix.Add(true);
+            mix.Add("chair");
+            int sum = 0;
+            foreach (var item in mix)
+            {
+                if (item is int)
+                {
+                    Console.WriteLine($"{item} is int");
+                    int num = (int)item;
+                    sum += num;
+                }
+                if(item is bool){
+                Console.WriteLine(item);
+                }
+                if(item is string){
+                Console.WriteLine(item);
+                }
+            }
+            Console.WriteLine($"Sum of list: {sum}");
+        }
+
+    }
+}
